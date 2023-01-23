66d15c8f082cd7ec7c3dfdbd8f09e713d4f27cee
Nikola Krstevski <nikolakrstevskids@gmail.com>
2023-01-23 15:42:30 +0100

    Creato il api funziona con json messo jsonignore in model pre evitare il load recursivo

---
+++ Controllers/PizzasController.cs
@@ -0,0 +1,22 @@
+﻿using LaMiaPizzeria.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LaMiaPizzeria.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PizzasController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            using( PizzaContext db =new PizzaContext() )
+            {
+                List<Pizza> pizzas = db.Pizza.Include(pizza => pizza.Ingridients).ToList<Pizza>(); 
+                return Ok( pizzas );
+            }
+        }
+    }
+}
--- Models/Ingridient.cs
+++ Models/Ingridient.cs
@@ -1,10 +1,13 @@
-﻿namespace LaMiaPizzeria.Models
+﻿using System.Text.Json.Serialization;
+
+namespace LaMiaPizzeria.Models
 {
     public class Ingridient
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public double Price { get; set; }
+        [JsonIgnore]
         public List<Pizza> Pizzas { get; set; }
 
         public Ingridient()
